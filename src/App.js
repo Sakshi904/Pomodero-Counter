58744cb20144f22e5e3273631bdedbd35537fa38
Sakshi gupta <sakshigupta@Apoorvs-MacBook-Pro.local>
2020-10-11 00:50:03 -0700

    Implemented start timer functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import React, { useState } from "react";
+function padTime(time) {
+  return time.toString().padStart(2, "0");
+}
+export default function App() {
+  const [timeleft, setTimeLeft] = useState(10);
+  const [beginText, setText] = useState("Let's begin");
+  const minutes = padTime(Math.floor(timeleft / 60));
+  const seconds = padTime(Math.floor(timeleft - minutes * 60));
+  function startTimer() {
+    setInterval(() => {
+      setTimeLeft((timeleft) => {
+        if (timeleft >= 1) return timeleft - 1;
+        else return 0;
+      });
+    }, 1000);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h2>{beginText}</h2>
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+      <div className="buttons">
+        <button onClick={startTimer}>Start </button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </div>
     </div>
   );
 }
-
-export default App;
